380a140a1476224b185c30b6d2021c96dffacd1c
Tommaso Castagnola <tommaso.castagnola@pomiager.com>
2024-04-27 20:11:06 +0200

    Last fix and routing trick

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 import { Subscription } from 'rxjs';
 import { UiService } from '../../services/ui.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
-    imports: [ButtonComponent]
+    imports: [ButtonComponent, CommonModule]
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
   showAddTask: boolean;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) { 
+  constructor(private uiService: UiService, private router: Router) { 
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
@@ -27,5 +29,9 @@ export class HeaderComponent {
 
   toggleAddTask() {
     this.uiService.toggleShowForm();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
