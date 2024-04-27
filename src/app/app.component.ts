50df95616e939cdd28e86326e0551dabdea0dfbf
Tommaso Castagnola <tommaso.castagnola@pomiager.com>
2024-04-27 19:58:40 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { TasksComponent } from "./components/tasks/tasks.component";
+import { FooterComponent } from "./components/footer/footer.component";
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, CommonModule, TasksComponent, HttpClientModule, FormsModule]
+  imports: [RouterOutlet,
+     HeaderComponent, CommonModule,
+      TasksComponent, FooterComponent, HttpClientModule, 
+      FormsModule]
 })
 
 export class AppComponent {
